87ce75f72a2874ba5e8ff7c4326551c37f8a4177
bryancuak <al20760361@ite.edu.mx>
2024-12-12 16:35:55 -0800

    actualizacion

--- Problema-3/code.js
+++ Problema-3/code.js
@@ -10,6 +10,7 @@ fs.readFile('palabra.txt', 'utf-8', (err, data) => {
     }
 
     const caracteres_Especiales = [';', ':', "'", '=', '(', ')', '"','*', '.', ' ', '@', '[', ']', '|', '\n', '\r',',']
+    const abcd = ['a', 'b', 'c', 'd', 'e', 'f']
     let arraySpliteado = [];
     let cadenasDeTexto = [];
     let palabra = "";
@@ -23,15 +24,36 @@ fs.readFile('palabra.txt', 'utf-8', (err, data) => {
                 // console.log(palabra);
                 palabra = "";
             }
+
             if (caracter !== ' ') {
                 arraySpliteado.push(caracter) //inserta todos los caracteres al final del arreglo
+
             }
+            
         }
         else{
             palabra += caracter; //con esto se concatena todo aquello que no es un caracter especial
         }
     }
     console.log(arraySpliteado);
+
+    for (let i = 0; i < arraySpliteado.length; i++) {
+        const palabras = arraySpliteado[i];
+        // console.log(palabras);
+        
+        for (let j = 0; j < palabras.length; j++) {
+            const letras = palabras[j]
+
+            // console.log(letras);
+            // console.log(arraySpliteado[j]);
+            
+            
+        }
+        // console.log(arraySpliteado[i]);
+        
+        
+        
+    }
 
 
 });
